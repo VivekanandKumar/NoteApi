71176a54351fb7a9ff0eb1e1fff3b250bb734b42
Vivekanand Kumar <mailofvivekanand@gmail.com>
2022-09-19 18:04:33 +0530

    43

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const {connectDb} = require('./db/Connection')
 const app = express()
 const {noteRoutes} = require('./routes/NoteRouter')
 const {userRoutes} = require('./routes/UserRouter')
-const cors = require('cors')
 // middlewares
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
